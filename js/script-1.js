392d1b800765aa1c37a5e255c031c1c3c2080334
YuriyBeregovoy <rik0681@gmail.com>
2023-04-17 16:49:34 +0300

    Update script-1.js

--- js/script-1.js
+++ js/script-1.js
@@ -4,3 +4,12 @@ const userName = "Chelsy";
 const message = 'Welcome';
 const isOpen = true;
 const shouldConfirm = false;
+
+
+
+console.log('ціна', totalPrice);
+
+const type = typeof true;
+console.log(type);
+
+alert(userName);
