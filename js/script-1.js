b33a11d259dce7128fbec97758f7752ad6f7955d
YuriyBeregovoy <rik0681@gmail.com>
2023-04-18 12:20:53 +0300

    Update script-1.js

--- js/script-1.js
+++ js/script-1.js
@@ -1,9 +1,9 @@
-const age = 10;
-const totalPrice = 200.74;
-const userName = "Chelsy";
-const message = 'Welcome';
-const isOpen = true;
-const shouldConfirm = false;
+// const age = 10;
+// const totalPrice = 200.74;
+// const userName = "Chelsy";
+// const message = 'Welcome';
+// const isOpen = true;
+// const shouldConfirm = false;
 
 // console.log('ціна', totalPrice);
 
@@ -22,3 +22,32 @@ const shouldConfirm = false;
 // let elementWidth = '50px;'
 // elementWidth = Number.parseInt(elementWidth);
 // console.log('elementWidth', elementWidth);
+
+// let elementWidth = '200.74px;'
+// elementWidth = Number.parseFloat(elementWidth);
+// console.log('elementWidth', elementWidth);
+
+// let salary = 1300.16472;
+// console.log(Number(salary.toFixed(2)));
+
+// console.log(Number(5/0));
+
+// const message = 'У цьому рядку 26 символівв.';
+// console.log(message.length);
+
+// console.log('gee' + 'dvjb');
+
+// const firstName = 'Chelsy';
+// const lastName = 'Emerald';
+// const fullName = firstName + lastName;
+// console.log(fullName);
+
+// const room = 716;
+// const type = 'VIP';
+
+// Довгий варіант (по старому):
+
+// const welcomeMsg = 'Гість ' + firstName + ' ' + lastName + ' заселяється в ' + type + ' номер ' + room;
+// Короткий варіант (по новому):
+// const welcomeMsg = `Гість ${firstName} ${lastName} заселяється у ${type} номер ${room}`;
+// console.log(welcomeMsg);
