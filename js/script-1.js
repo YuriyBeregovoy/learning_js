c0416bba2affba6894687bd58c71b44d546e5363
YuriyBeregovoy <rik0681@gmail.com>
2023-04-18 17:41:41 +0300

    Update script-1.js

--- js/script-1.js
+++ js/script-1.js
@@ -127,3 +127,15 @@ let number = 15;
 // 25 < 10 || 25 > 30
 // false || false
 // false
+
+// Напишіть скрипт, який перевіряє можливість відкрити чат з користувачем.
+// Для цього користувач повинен бути:
+// -другом
+// - онлайн
+// - без режиму не турбувати
+
+// const isOnline = true; 
+// const isFriend = true;
+// const isDnd = false;
+// const canOpenChat = isOnline && isFriend && !isDnd;
+// console.log('Можна відкрити чат? ', canOpenChat);
