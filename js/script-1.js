4fa43f5d0add79ff8c3a074eea421bcf8bd70f94
YuriyBeregovoy <rik0681@gmail.com>
2023-04-20 18:35:50 +0300

    Update script-1.js

--- js/script-1.js
+++ js/script-1.js
@@ -284,3 +284,17 @@
 // console.log(false === "");
 
 // КІНЕЦЬ ЗАНЯТТЯ №1////////////////////////////////////////////////////////////////////
+
+
+
+// ФУНКЦІЯ//////////////////////////
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   // Change code below this line
+
+// const message = `You ordered droids worth ${orderedQuantity * pricePerDroid + deliveryFee} credits. Delivery (${deliveryFee} credits) is included in total price.`
+
+//   // Change code above this line
+//   return message;
+// }
+// console.log(makeOrderMessage(2, 100, 50));
