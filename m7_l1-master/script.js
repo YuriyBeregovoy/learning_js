79d3632f9e2dd97d9708642bb22062d6824281e5
YuriyBeregovoy <rik0681@gmail.com>
2023-06-17 12:54:44 +0300

    Update script.js

--- m7_l1-master/script.js
+++ m7_l1-master/script.js
@@ -1,44 +1,45 @@
+
 const productFormEl = document.getElementById("productForm");
 const priceInputEl = document.querySelector("input[name='price']");
 const quantityInputEl = document.querySelector("input[name='quantity']");
 const totalPriceEl = document.getElementById("totalPrice");
 const amountEl = document.getElementById("amount");
 
-const changeTotalPrice = () => {
-  totalPriceEl.textContent = priceInputEl.value * quantityInputEl.value + "uah";
-};
+// const changeTotalPrice = () => {
+//   totalPriceEl.textContent = priceInputEl.value * quantityInputEl.value + "uah";
+// };
 
-const productFormHandler = (event) => {
-  event.preventDefault();
-  const inputName = event.target.name;
+// const productFormHandler = (event) => {
+//   event.preventDefault();
+//   const inputName = event.target.name;
 
-  if (inputName === "quantity") {
-    amountEl.textContent = quantityInputEl.value;
-  }
+//   if (inputName === "quantity") {
+//     amountEl.textContent = quantityInputEl.value;
+//   }
 
-  changeTotalPrice();
-};
+//   changeTotalPrice();
+// };
 
-productFormEl.addEventListener("input", productFormHandler);
-changeTotalPrice();
+// productFormEl.addEventListener("input", productFormHandler);
+// changeTotalPrice();
 
-const parent = document.getElementById("parent");
-const child = document.getElementById("child");
-const descendant = document.getElementById("descendant");
+// const parent = document.getElementById("parent");
+// const child = document.getElementById("child");
+// const descendant = document.getElementById("descendant");
 
-parent.addEventListener("click", (event) => {
-  console.log("parent handler", event.target.id);
-  console.log("parent handler", event.currentTarget.id);
-});
+// parent.addEventListener("click", (event) => {
+//   console.log("parent handler", event.target.id);
+//   console.log("parent handler", event.currentTarget.id);
+// });
 
-child.addEventListener("click", (event) => {
-  event.stopPropagation();
+// child.addEventListener("click", (event) => {
+//   event.stopPropagation();
 
-  console.log("child handler", event.target.id);
-  console.log("child handler", event.currentTarget.id);
-});
+//   console.log("child handler", event.target.id);
+//   console.log("child handler", event.currentTarget.id);
+// });
 
-descendant.addEventListener("click", (event) => {
-  console.log("descendant handler", event.target.id);
-  console.log("descendant handler", event.currentTarget.id);
-});
+// descendant.addEventListener("click", (event) => {
+//   console.log("descendant handler", event.target.id);
+//   console.log("descendant handler", event.currentTarget.id);
+// });
